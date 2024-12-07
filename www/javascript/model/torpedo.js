cc584e811ca1ff5b16291c4ff015e8b8c570eead
babyloos <babyloos2501@gmail.com>
2024-12-08 02:37:20 +0900

    魚雷衝突時のコールバックを削除
    - enemyShipsに衝突時のコールバックを設定する

--- www/javascript/model/torpedo.js
+++ www/javascript/model/torpedo.js
@@ -14,8 +14,6 @@ export class Torpedo extends GameObject {
 
     depth = 0;
 
-    onHitTorpedoCallback = null;
-
     /**
      * コンストラクタ
      * @param {double} pointX X座標
@@ -97,11 +95,6 @@ export class Torpedo extends GameObject {
     onHitTorpedo() {
         super.onHitTorpedo();
         this.isEnabled = false;
-        this.onHitTorpedoCallback();
-    }
-
-    setOnHitTorpedoCallback(callback) {
-        this.onHitTorpedoCallback = callback;
     }
 
     /**
