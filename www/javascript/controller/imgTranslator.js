0ec9720534e853cf77f804a04b7a5e9b514178e7
babyloos <babyloos2501@gmail.com>
2024-12-15 17:09:43 +0900

    画像置換動作確認

--- www/javascript/controller/imgTranslator.js
+++ www/javascript/controller/imgTranslator.js
@@ -5,31 +5,16 @@ export default class ImgTranslator {
     static async translate() {
         const language = navigator.language;
         console.log(language);
-        const json = await this.getJson();
 
         if (language !== 'ja') {
-            // 日本語版出ない場合は何もしない
+            // 日本語版でない場合は何もしない
             return;
         }
 
         // translateのついているimgタグのsrcを書き換える
         const imgElements = document.querySelectorAll('img[translate]');
-        console.log(imgElements);
-    }
-
-    static async getJson() {
-        return await fetch('./resources/imgSrc.json')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('HTTP error! status: ' + response.status);
-                }
-                return response.json();
-            })
-            .then(jsonData => {
-                return jsonData;
-            })
-            .catch(error => {
-                console.error('jsonファイル取得エラー:', error);
-            });
+        imgElements.forEach((imgElement) => {
+            imgElement.src = imgElement.src.replace('resources/img/', `resources/img/${language}/`);
+        });
     }
 }
