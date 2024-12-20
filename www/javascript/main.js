dec58b784afe496d4bab0e1d5ca8d87b615e9236
babyloos <babyloos2501@gmail.com>
2024-12-20 22:29:30 +0900

    admob表示のデバッグコード追加

--- www/javascript/main.js
+++ www/javascript/main.js
@@ -186,5 +186,41 @@ window.addEventListener('DOMContentLoaded', function () {
     ImgTranslator.translate();
 })
 
+// const showAd = async () => {
+//     const interstitial = new admob.InterstitialAd({
+//         adUnitId: 'ca-app-pub-3940256099942544/1033173712',
+//     })
+
+//     interstitial.on('load', (evt) => {
+//         // evt.ad
+//     })
+
+//     await interstitial.load()
+//     await interstitial.show()
+// }
+
+// showAd();
+
+document.addEventListener('deviceready', async () => {
+    console.log('device ready');
+    interstitial = new admob.InterstitialAd({
+        // iOS test
+        adUnitId: 'ca-app-pub-3940256099942544/4411468910',
+    })
+
+    interstitial.on('load', (evt) => {
+        // evt.ad
+    })
+
+    await interstitial.load()
+    await interstitial.show()
+}, false);
+
+window.addEventListener('admob.ad.dismiss', async () => {
+    // Once a interstitial ad is shown, it cannot be shown again.
+    // Starts loading the next interstitial ad as soon as it is dismissed.
+    await interstitial.load()
+})
+
 const main = new Main();
 main.main();
